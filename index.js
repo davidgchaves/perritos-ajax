323da4b747f12b5945050dbf305c373fdb36548c
David G Chaves <davidgchaves@gmail.com>
2018-10-30 13:35:45 +0100

    Can access dog URL sent from API

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
-fetch(RANDOM_DOG_URL).then(function(apiResponse) {
-  console.log(apiResponse.json());
-});
+fetch(RANDOM_DOG_URL)
+  .then(function(apiResponse) {
+    return apiResponse.json();
+  })
+  .then(function(jsonResponse) {
+    console.log(jsonResponse.message);
+  });
